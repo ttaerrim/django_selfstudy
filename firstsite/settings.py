9d481fb4ea2a278fcfbbaeac7bb391bfee1285b5
Lee <ltr0121@naver.com>
2020-06-06 02:56:06 +0900

    allowed hosts에 '*' 추가

--- firstsite/settings.py
+++ firstsite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'zcd2!a#9lwcr69yl-_g2)!5%n(k+1d
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = bool( os.environ.get('DJANGO_DEBUG', True) )
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
